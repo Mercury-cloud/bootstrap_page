d4e280aa64df3ca20377fee32e00cb2f26ca5720
Mercury <drydev033@gmail.com>
2020-05-07 04:50:13 +0900

    Color Process finished

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ $(()=>{
         clon.children[0].children[0].children[0].children[0].classList = "fas fa-5x main_icon " + icon_name[index%icon_name.length];
         clon.children[0].children[0].children[0].children[0].style.color = icon_color[index%icon_color.length];
         $('.flex_div').append(clon);
-        
     }
-    
+    let tempColor;
+    $('.box').hover(        
+        function(){
+            tempColor = $(this).find('.main_icon').css('color');
+            $(this).find('.main_icon').css('color', '#FFFFFF');
+        },
+        function(){
+            $(this).find('.main_icon').css('color', tempColor);
+        }    
+    );
 
 });
